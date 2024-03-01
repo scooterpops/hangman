ba7b5c101bf219add5b70bdb8819a600af780c1b
‘Ricky <rjvicenzi@gmail.com>
2024-03-01 11:58:36 -0500

    post presentation push

--- README.md
+++ README.md
@@ -5,8 +5,13 @@
 
 The classic game of HANGMAN. Player 1 chooses a word and Player 2 tries to guess the word 1 letter at a time. If Player 2 correctly guesses the word before their 5 wrong guesses are used up, Player 2 wins. Otherwise, Player 1 is the champion.
 
+GAMEPLAY
+<img src="https://i.imgur.com/99WRIN7.jpeg" width="800" height="400">
 
 TECHNOLOGIES USED
 -HTML
 -CSS
 -JavaScript
+
+NEXT STEPS
+-P1 vs COMPUTER
--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
-/*----- constants -----*/
-
-/*----- state variables -----*/
 let wordToGuess
 let letterToGuess
 let wordToGuessArray = []
+let lettersGuessedArray =[]
 let mistakesLeft = 5
 let mistakesMade = 0
-let lettersGuessedArray =[]
+
 
 document.querySelector('.counter').innerText = mistakesLeft
 
-/*----- cached elements  -----*/
 const wordToGuessInput = document.querySelector('#wordToGuess')
 const letterToGuessInput = document.querySelector('#letterToGuess')
 const guessesLeftElement = document.querySelector('.counter')
-
-/*----- event listeners -----*/
-
-/*----- functions -----*/
 
 function init() {
 }
 
 function getWordToGuess() {
     wordToGuess = wordToGuessInput.value.toUpperCase()
-    console.log(wordToGuess)
     wordToGuessArray = wordToGuess.split("")
     wordToGuessInput.value = ''
     wordToGuessInput.disabled = true
-    console.log(wordToGuessArray)
     
     if (wordToGuessArray.length < 7) {
         for (let i = 6; i >= wordToGuessArray.length; i--) {
@@ -39,21 +30,17 @@ function getWordToGuess() {
 
 function checkLetterToGuess() {
     letterToGuess = letterToGuessInput.value.toUpperCase()
-    console.log(letterToGuess)
     
     if (!lettersGuessedArray.includes(letterToGuess)) {
         lettersGuessedArray.push(letterToGuess);
     } 
-    
     if (wordToGuessArray.includes(letterToGuess)){
-        console.log("correct guess")
         wordToGuessArray.forEach(function(letter, idx) {
             if (letter === letterToGuess) {
             document.getElementById('l' + idx).innerText = letter.toUpperCase()
             }
         })
     } else {
-        console.log("incorrect guess")
         document.getElementById('v' + mistakesMade).innerText = letterToGuess.toUpperCase()
         mistakesMade++
         mistakesLeft--
@@ -62,31 +49,18 @@ function checkLetterToGuess() {
 
     letterToGuessInput.value = ''
 
-    console.log(wordToGuess)
-    console.log(mistakesLeft)
-    console.log(mistakesMade)
-    console.log(wordToGuessArray)
-    console.log(lettersGuessedArray)
-
     function checkWinner(wordToGuessArray, lettersGuessedArray) {
         return wordToGuessArray.every(element => lettersGuessedArray.includes(element));
-        //checks to see if all elements in wordToGuessArray are in lettersGuessedArray
     }
       if (checkWinner(wordToGuessArray, lettersGuessedArray)) {
-        console.log("P2 WINS");
         let blackout = document.getElementById("blackout")
         blackout.classList.remove("hide")
         blackout.classList.add("blackout")
         document.querySelector(".win-message").innerText = "P2 WINS"
-        //if P2 wins, remove 'hide' class from 'blackout' id, and change win-message
        } else if (mistakesLeft === 0) {
-        console.log("P1 WINS");
         let blackout = document.getElementById("blackout")
         blackout.classList.remove("hide")
         blackout.classList.add("blackout")
         document.querySelector(".win-message").innerText = "P1 WINS"
-        //if P1 wins, remove 'hide' class from 'blackout' id, and change win-message
        }
-        else {console.log("NO WINNER YET");
-       } 
-}
+} 
