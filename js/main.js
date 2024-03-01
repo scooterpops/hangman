b45825bfff6be84b63575cbf03d5f30d2a84b3d1
‘Ricky <rjvicenzi@gmail.com>
2024-02-29 19:49:36 -0500

    testing readme

--- js/main.js
+++ js/main.js
@@ -9,19 +9,15 @@ let mistakesMade = 0
 let lettersGuessedArray =[]
 
 document.querySelector('.counter').innerText = mistakesLeft
-// document.querySelector('#blackout').innerHTML = 
 
 /*----- cached elements  -----*/
 const wordToGuessInput = document.querySelector('#wordToGuess')
 const letterToGuessInput = document.querySelector('#letterToGuess')
 const guessesLeftElement = document.querySelector('.counter')
 
-
 /*----- event listeners -----*/
 
 /*----- functions -----*/
-
-// init()
 
 function init() {
 }
@@ -36,18 +32,20 @@ function getWordToGuess() {
 function checkLetterToGuess() {
     letterToGuess = letterToGuessInput.value.toUpperCase()
     console.log(letterToGuess)
+    
     if (!lettersGuessedArray.includes(letterToGuess)) {
         lettersGuessedArray.push(letterToGuess);
     } 
+    
     if (wordToGuessArray.includes(letterToGuess)){
-        console.log("true")
+        console.log("correct guess")
         wordToGuessArray.forEach(function(letter, idx) {
             if (letter === letterToGuess) {
             document.getElementById('l' + idx).innerText = letter.toUpperCase()
             }
         })
     } else {
-        console.log("false")
+        console.log("incorrect guess")
         document.getElementById('v' + mistakesMade).innerText = letterToGuess.toUpperCase()
         mistakesMade++
         mistakesLeft--
@@ -62,33 +60,23 @@ function checkLetterToGuess() {
 
     function checkWinner(wordToGuessArray, lettersGuessedArray) {
         return wordToGuessArray.every(element => lettersGuessedArray.includes(element));
-      }
+        //checks to see if all elements in wordToGuessArray are in lettersGuessedArray
+    }
       if (checkWinner(wordToGuessArray, lettersGuessedArray)) {
         console.log("P2 WINS");
         let blackout = document.getElementById("blackout")
         blackout.classList.remove("hide")
         blackout.classList.add("blackout")
         document.querySelector(".win-message").innerText = "P2 WINS"
+        //if P2 wins, remove 'hide' class from 'blackout' id, and change win-message
        } else if (mistakesLeft === 0) {
         console.log("P1 WINS");
         let blackout = document.getElementById("blackout")
         blackout.classList.remove("hide")
         blackout.classList.add("blackout")
         document.querySelector(".win-message").innerText = "P1 WINS"
+        //if P1 wins, remove 'hide' class from 'blackout' id, and change win-message
        }
         else {console.log("NO WINNER YET");
-
        } 
 }
-
-
-
-// function getLetterToGuess() {
-//     letterToGuess = letterToGuessInput.value
-//     console.log(letterToGuess)
-// }
-
-// function updateGuessesLeft() {
-//     guessesLeftElement.innerHTML = guessesLeft
-//     console.log(guessesLeftElement)
-//
